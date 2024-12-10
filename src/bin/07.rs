6678c913c0b54af230ecfda561b2e17e2143f6b1
Nuno Sousa <nuno.mt.sousa@gmail.com>
2024-12-10 18:09:24 +0000

    Day 7, part II.

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -26,6 +26,13 @@ enum Operation {
     Multiply,
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum Operation2 {
+    Sum,
+    Multiply,
+    Concatenate,
+}
+
 fn main() -> Result<()> {
     start_day(DAY);
 
@@ -82,20 +89,69 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let mut sum_of_test_values: usize = 0;
+        for line in reader.lines().map_while(Result::ok) {
+            let mut line_split = line.split_whitespace();
+            let total = line_split
+                .next()
+                .expect("Expected first number in line")
+                .split(":")
+                .next()
+                .expect("Number before :")
+                .parse::<usize>()
+                .unwrap();
+
+            let parts: Vec<usize> = line_split.flat_map(|n| n.parse::<usize>()).collect();
+            let mut operations = vec![Operation2::Sum; parts.len() - 1];
+            let mut missing_mutations = true;
+            while missing_mutations {
+                let mut result;
+
+                match operations[0] {
+                    Operation2::Sum => result = parts[0] + parts[1],
+                    Operation2::Multiply => result = parts[0] * parts[1],
+                    Operation2::Concatenate => result = concatenate(parts[0], parts[1]),
+                }
+
+                for i in 1..operations.len() {
+                    match operations[i] {
+                        Operation2::Sum => result += parts[i + 1],
+                        Operation2::Multiply => result *= parts[i + 1],
+                        Operation2::Concatenate => result = concatenate(result, parts[i + 1]),
+                    }
+                }
+                if result == total {
+                    sum_of_test_values += total;
+                    break;
+                }
+
+                missing_mutations = mutate_operations2(&mut operations);
+            }
+        }
+        Ok(sum_of_test_values)
+    }
+
+    assert_eq!(11387, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
+}
+
+fn concatenate(a: usize, b: usize) -> usize {
+    let astr = a.to_string();
+    let bstr = b.to_string();
+
+    let mut result: String = astr.clone();
+    result.push_str(&bstr);
+
+    result.parse::<usize>().expect("Number is parsable")
 }
 
 fn mutate_operations(operations: &mut [Operation]) -> bool {
@@ -121,7 +177,45 @@ fn mutate_operations(operations: &mut [Operation]) -> bool {
     true
 }
 
+fn mutate_operations2(operations: &mut [Operation2]) -> bool {
+    if all_are2(operations, Operation2::Concatenate) {
+        return false;
+    }
+
+    match operations[0] {
+        Operation2::Sum => operations[0] = Operation2::Multiply,
+        Operation2::Multiply => operations[0] = Operation2::Concatenate,
+        Operation2::Concatenate => {
+            operations[0] = Operation2::Sum;
+            for i in 1..operations.len() {
+                match operations[i] {
+                    Operation2::Sum => {
+                        operations[i] = Operation2::Multiply;
+                        break;
+                    }
+                    Operation2::Multiply => {
+                        operations[i] = Operation2::Concatenate;
+                        break;
+                    }
+                    Operation2::Concatenate => operations[i] = Operation2::Sum,
+                }
+            }
+        }
+    }
+    true
+}
+
 fn all_are(operations: &[Operation], operation: Operation) -> bool {
+    for op in operations {
+        if *op != operation {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn all_are2(operations: &[Operation2], operation: Operation2) -> bool {
     for op in operations {
         if *op != operation {
             return false;
