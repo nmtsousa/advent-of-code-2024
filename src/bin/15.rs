eebd14682adabfd7894bd63181210ff29ee1ce02
Nuno Sousa <nuno.mt.sousa@gmail.com>
2024-12-15 16:24:32 +0000

    Day 15, part II, some bug is preventing me from finishing it.

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -45,6 +45,18 @@ vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
 v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
 ";
 
+const TEST_3: &str = "\
+#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^
+";
+
 fn main() -> Result<()> {
     start_day(DAY);
 
@@ -81,17 +93,38 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let mut lines = reader.lines().map_while(Result::ok);
+        let mut wharehouse = WharehouseWide::new(&mut lines);
+
+        // wharehouse.dump_state();
+        for line in lines.by_ref() {
+            line.chars().for_each(|c| {
+                // println!("Move: {}", c);
+                match c {
+                    '<' => wharehouse.move_left(),
+                    '>' => wharehouse.move_right(),
+                    '^' => wharehouse.move_up(),
+                    'v' => wharehouse.move_down(),
+                    c => panic!("Unexpected motion [{}]!", c),
+                };
+                // wharehouse.dump_state();
+                wharehouse.validate_state();
+            });
+        }
+
+        Ok(wharehouse.sum_gps())
+    }
+
+    assert_eq!(1751, part2(BufReader::new(TEST_1.as_bytes()))?);
+    assert_eq!(9021, part2(BufReader::new(TEST_2.as_bytes()))?);
+    assert_eq!(618, part2(BufReader::new(TEST_3.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
@@ -259,6 +292,232 @@ impl Wharehouse {
                     })
                     .collect::<String>()
             );
+        });
+    }
+}
+
+#[derive(Debug)]
+struct WharehouseWide {
+    map: Vec<Vec<TileWide>>,
+    robot_x: usize,
+    robot_y: usize,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum TileWide {
+    Wall,     // #
+    Free,     // .
+    BoxLeft,  // [
+    BoxRight, // ]
+}
+
+impl WharehouseWide {
+    fn new(lines: &mut impl Iterator<Item = String>) -> Self {
+        let mut map = vec![];
+        let mut robot_x = None;
+        let mut robot_y = None;
+
+        for (y, line) in lines.by_ref().enumerate() {
+            if line.is_empty() {
+                break;
+            }
+
+            map.push(
+                line.chars()
+                    .flat_map(|c| {
+                        match c {
+                            '#' => "##",
+                            'O' => "[]",
+                            '.' => "..",
+                            '@' => "@.",
+                            _ => panic!("Unexpected input to double."),
+                        }
+                        .chars()
+                    })
+                    .enumerate()
+                    .map(|(x, c)| match c {
+                        '#' => TileWide::Wall,
+                        '.' => TileWide::Free,
+                        '[' => TileWide::BoxLeft,
+                        ']' => TileWide::BoxRight,
+                        '@' => {
+                            robot_x = Some(x);
+                            robot_y = Some(y);
+                            TileWide::Free
+                        }
+                        _ => panic!("Unexpected tile"),
+                    })
+                    .collect(),
+            );
+        }
+
+        Self {
+            map,
+            robot_x: robot_x.expect("Robot X"),
+            robot_y: robot_y.expect("Rboot Y"),
+        }
+    }
+
+    fn move_left(&mut self) {
+        if self.attemp_left(self.robot_x - 1, self.robot_y) {
+            self.robot_x -= 1;
+        }
+    }
+
+    fn attemp_left(&mut self, target_x: usize, target_y: usize) -> bool {
+        match self.get_at(target_x, target_y) {
+            TileWide::Free => true,
+            TileWide::Wall => false,
+            t if self.attemp_left(target_x - 1, target_y) => {
+                self.map[target_y][target_x - 1] = t;
+                self.map[target_y][target_x] = TileWide::Free;
+                true
+            }
+            _ => false,
+        }
+    }
+
+    fn get_at(&self, target_x: usize, target_y: usize) -> TileWide {
+        self.map[target_y][target_x]
+    }
+
+    fn move_right(&mut self) {
+        if self.attemp_right(self.robot_x + 1, self.robot_y) {
+            self.robot_x += 1;
+        }
+    }
+
+    fn attemp_right(&mut self, target_x: usize, target_y: usize) -> bool {
+        match self.get_at(target_x, target_y) {
+            TileWide::Free => true,
+            TileWide::Wall => false,
+            t if self.attemp_right(target_x + 1, target_y) => {
+                self.map[target_y][target_x + 1] = t;
+                self.map[target_y][target_x] = TileWide::Free;
+                true
+            }
+            _ => false,
+        }
+    }
+
+    fn move_up(&mut self) {
+        if self.attemp_up(self.robot_x, self.robot_y - 1) {
+            self.robot_y -= 1;
+        }
+    }
+
+    fn attemp_up(&mut self, target_x: usize, target_y: usize) -> bool {
+        match self.get_at(target_x, target_y) {
+            TileWide::Free => true,
+            TileWide::Wall => false,
+            TileWide::BoxLeft
+                if self.attemp_up(target_x, target_y - 1)
+                    && self.attemp_up(target_x + 1, target_y - 1) =>
+            {
+                self.map[target_y - 1][target_x] = TileWide::BoxLeft;
+                self.map[target_y][target_x] = TileWide::Free;
+                self.map[target_y - 1][target_x + 1] = TileWide::BoxRight;
+                self.map[target_y][target_x + 1] = TileWide::Free;
+                true
+            }
+            TileWide::BoxRight
+                if self.attemp_up(target_x, target_y - 1)
+                    && self.attemp_up(target_x - 1, target_y - 1) =>
+            {
+                self.map[target_y - 1][target_x] = TileWide::BoxRight;
+                self.map[target_y][target_x] = TileWide::Free;
+                self.map[target_y - 1][target_x - 1] = TileWide::BoxLeft;
+                self.map[target_y][target_x - 1] = TileWide::Free;
+                true
+            }
+            _ => false,
+        }
+    }
+
+    fn move_down(&mut self) {
+        if self.attemp_down(self.robot_x, self.robot_y + 1) {
+            self.robot_y += 1;
+        }
+    }
+
+    fn attemp_down(&mut self, target_x: usize, target_y: usize) -> bool {
+        match self.get_at(target_x, target_y) {
+            TileWide::Free => true,
+            TileWide::Wall => false,
+            TileWide::BoxLeft
+                if self.attemp_down(target_x, target_y + 1)
+                    && self.attemp_down(target_x + 1, target_y + 1) =>
+            {
+                self.map[target_y + 1][target_x] = TileWide::BoxLeft;
+                self.map[target_y][target_x] = TileWide::Free;
+                self.map[target_y + 1][target_x + 1] = TileWide::BoxRight;
+                self.map[target_y][target_x + 1] = TileWide::Free;
+                true
+            }
+            TileWide::BoxRight
+                if self.attemp_down(target_x, target_y + 1)
+                    && self.attemp_down(target_x - 1, target_y + 1) =>
+            {
+                self.map[target_y + 1][target_x] = TileWide::BoxRight;
+                self.map[target_y][target_x] = TileWide::Free;
+                self.map[target_y + 1][target_x - 1] = TileWide::BoxLeft;
+                self.map[target_y][target_x - 1] = TileWide::Free;
+                true
+            }
+            _ => false,
+        }
+    }
+
+    fn sum_gps(&self) -> usize {
+        let mut result = 0;
+
+        self.map.iter().enumerate().for_each(|(row_index, row)| {
+            row.iter().enumerate().for_each(|(col_index, tile)| {
+                if *tile == TileWide::BoxLeft {
+                    result += row_index * 100 + col_index;
+                }
+            });
+        });
+
+        result
+    }
+
+    fn dump_state(&self) {
+        self.map.iter().enumerate().for_each(|(r_index, r)| {
+            println!(
+                "{}",
+                r.iter()
+                    .enumerate()
+                    .map(|(t_index, t)| {
+                        if r_index == self.robot_y && t_index == self.robot_x {
+                            return '@';
+                        }
+                        match t {
+                            TileWide::BoxLeft => '[',
+                            TileWide::BoxRight => ']',
+                            TileWide::Free => '.',
+                            TileWide::Wall => '#',
+                        }
+                    })
+                    .collect::<String>()
+            );
+        });
+    }
+
+    fn validate_state(&self) {
+        let mut expected = None;
+        self.map.iter().for_each(|r| {
+            r.iter().for_each(|t| match expected {
+                Some(e) => {
+                    assert_eq!(e, *t);
+                    expected = None;
+                }
+                None => match t {
+                    TileWide::BoxLeft => expected = Some(TileWide::BoxRight),
+                    TileWide::BoxRight => panic!("Unexpected right box"),
+                    _ => (),
+                },
+            });
         });
     }
 }
