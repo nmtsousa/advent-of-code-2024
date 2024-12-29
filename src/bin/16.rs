a1df162dc8a6caa7987c7d9bf7083e1c06eddd6d
Nuno Sousa <nuno.mt.sousa@gmail.com>
2024-12-29 12:09:37 +0000

    Reading input puzzle.

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -54,10 +54,10 @@ fn main() -> Result<()> {
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
         let mut lines = reader.lines().map_while(Result::ok);
-        let puzzle = Puzzle::new(lines);
+        let puzzle = Puzzle::new(&mut lines);
         // TODO: Solve Part 1 of the puzzle
-        let answer = reader.lines().flatten().count();
-        Ok(answer)
+        puzzle.dump_state();
+        Ok(puzzle.solve())
     }
 
     assert_eq!(7036, part1(BufReader::new(TEST_1.as_bytes()))?);
@@ -85,10 +85,77 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-struct Puzzle {}
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Tile {
+    Wall,        // #
+    Free,        // .
+    Start,       // S
+    End,         // E
+    Cost(usize), // Cost to reach tile
+}
+
+struct Puzzle {
+    map: Vec<Vec<Tile>>,
+    start_x: usize,
+    start_y: usize,
+}
 
 impl Puzzle {
     fn new(lines: &mut impl Iterator<Item = String>) -> Self {
-        Self {}
+        let mut map = vec![];
+        let mut start_x = None;
+        let mut start_y = None;
+
+        for (y, line) in lines.by_ref().enumerate() {
+            if line.is_empty() {
+                break;
+            }
+            map.push(
+                line.chars()
+                    .enumerate()
+                    .map(|(x, c)| match c {
+                        '#' => Tile::Wall,
+                        '.' => Tile::Free,
+                        'S' => {
+                            start_x = Some(x);
+                            start_y = Some(y);
+                            Tile::Start
+                        }
+                        'E' => Tile::End,
+                        _ => panic!("Unexpected tile"),
+                    })
+                    .collect(),
+            );
+        }
+
+        Self {
+            map,
+            start_x: start_x.expect("Start X was found."),
+            start_y: start_y.expect("Start Y was found."),
+        }
+    }
+
+    fn dump_state(&self) {
+        self.map.iter().enumerate().for_each(|(r_index, r)| {
+            println!(
+                "{}",
+                r.iter()
+                    .enumerate()
+                    .map(|(t_index, t)| {
+                        match t {
+                            Tile::Wall => '#',
+                            Tile::Free => '.',
+                            Tile::Start => 'S',
+                            Tile::End => 'E',
+                            Tile::Cost(_) => '+',
+                        }
+                    })
+                    .collect::<String>()
+            );
+        });
+    }
+
+    fn solve(&self) -> usize {
+        todo!()
     }
 }
