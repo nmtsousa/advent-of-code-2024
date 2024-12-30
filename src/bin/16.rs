527e8ff9bbff9e818a0eafa9e41ceabf7110e1d2
Nuno Sousa <nuno.mt.sousa@gmail.com>
2024-12-30 18:05:54 +0000

    Day 16, part I.

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -54,10 +54,9 @@ fn main() -> Result<()> {
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
         let mut lines = reader.lines().map_while(Result::ok);
-        let puzzle = Puzzle::new(&mut lines);
-        // TODO: Solve Part 1 of the puzzle
-        puzzle.dump_state();
-        Ok(puzzle.solve())
+        let mut puzzle = Puzzle::new(&mut lines);
+        let result = puzzle.solve();
+        Ok(result)
     }
 
     assert_eq!(7036, part1(BufReader::new(TEST_1.as_bytes()))?);
@@ -98,6 +97,156 @@ struct Puzzle {
     map: Vec<Vec<Tile>>,
     start_x: usize,
     start_y: usize,
+    lowest_cost: Option<usize>,
+}
+
+#[derive(Debug)]
+struct State {
+    x: usize,
+    y: usize,
+    direction: Direction,
+    cost: usize,
+}
+
+#[derive(Debug)]
+enum Direction {
+    East,
+    North,
+    West,
+    South,
+}
+
+const FORWARD_COST: usize = 1;
+const TURN_COST: usize = 1000;
+
+impl State {
+    fn new_start(x: usize, y: usize) -> Self {
+        Self {
+            x,
+            y,
+            direction: Direction::East,
+            cost: 0,
+        }
+    }
+
+    fn move_forward(&self) -> State {
+        match self.direction {
+            Direction::East => State {
+                x: self.x - 1,
+                y: self.y,
+                direction: Direction::East,
+                cost: self.cost + FORWARD_COST,
+            },
+            Direction::North => State {
+                x: self.x,
+                y: self.y - 1,
+                direction: Direction::North,
+                cost: self.cost + FORWARD_COST,
+            },
+            Direction::West => State {
+                x: self.x + 1,
+                y: self.y,
+                direction: Direction::West,
+                cost: self.cost + FORWARD_COST,
+            },
+            Direction::South => Self {
+                x: self.x,
+                y: self.y + 1,
+                direction: Direction::South,
+                cost: self.cost + FORWARD_COST,
+            },
+        }
+    }
+
+    fn turn_left(&self) -> State {
+        let new_cost = self.cost + TURN_COST + FORWARD_COST;
+        match self.direction {
+            Direction::East => State {
+                x: self.x,
+                y: self.y + 1,
+                direction: Direction::South,
+                cost: new_cost,
+            },
+            Direction::North => State {
+                x: self.x - 1,
+                y: self.y,
+                direction: Direction::East,
+                cost: new_cost,
+            },
+            Direction::West => State {
+                x: self.x,
+                y: self.y - 1,
+                direction: Direction::North,
+                cost: new_cost,
+            },
+            Direction::South => State {
+                x: self.x + 1,
+                y: self.y,
+                direction: Direction::West,
+                cost: new_cost,
+            },
+        }
+    }
+
+    fn turn_right(&self) -> State {
+        let new_cost = self.cost + TURN_COST + FORWARD_COST;
+        match self.direction {
+            Direction::East => State {
+                x: self.x,
+                y: self.y - 1,
+                direction: Direction::North,
+                cost: new_cost,
+            },
+            Direction::North => State {
+                x: self.x + 1,
+                y: self.y,
+                direction: Direction::West,
+                cost: new_cost,
+            },
+            Direction::West => State {
+                x: self.x,
+                y: self.y + 1,
+                direction: Direction::South,
+                cost: new_cost,
+            },
+            Direction::South => State {
+                x: self.x - 1,
+                y: self.y,
+                direction: Direction::East,
+                cost: new_cost,
+            },
+        }
+    }
+
+    fn u_turn(&self) -> State {
+        let new_cost = self.cost + TURN_COST + FORWARD_COST;
+        match self.direction {
+            Direction::East => State {
+                x: self.x + 1,
+                y: self.y,
+                direction: Direction::West,
+                cost: new_cost,
+            },
+            Direction::North => State {
+                x: self.x,
+                y: self.y + 1,
+                direction: Direction::South,
+                cost: new_cost,
+            },
+            Direction::West => State {
+                x: self.x - 1,
+                y: self.y,
+                direction: Direction::East,
+                cost: new_cost,
+            },
+            Direction::South => State {
+                x: self.x,
+                y: self.y - 1,
+                direction: Direction::North,
+                cost: new_cost,
+            },
+        }
+    }
 }
 
 impl Puzzle {
@@ -132,16 +281,16 @@ impl Puzzle {
             map,
             start_x: start_x.expect("Start X was found."),
             start_y: start_y.expect("Start Y was found."),
+            lowest_cost: None,
         }
     }
 
     fn dump_state(&self) {
-        self.map.iter().enumerate().for_each(|(r_index, r)| {
+        self.map.iter().for_each(|r| {
             println!(
                 "{}",
                 r.iter()
-                    .enumerate()
-                    .map(|(t_index, t)| {
+                    .map(|t| {
                         match t {
                             Tile::Wall => '#',
                             Tile::Free => '.',
@@ -155,7 +304,34 @@ impl Puzzle {
         });
     }
 
-    fn solve(&self) -> usize {
-        todo!()
+    fn solve(&mut self) -> usize {
+        // Start has a cost of 0
+
+        let start = State::new_start(self.start_x, self.start_y);
+        self.solve_for(start);
+
+        self.lowest_cost.expect("Found lowest cost path.")
+    }
+
+    fn solve_for(&mut self, state: State) {
+        match self.map[state.y][state.x] {
+            Tile::Wall => return,
+            Tile::Free => self.map[state.y][state.x] = Tile::Cost(state.cost),
+            Tile::Start => (),
+            Tile::Cost(x) if state.cost < x => self.map[state.y][state.x] = Tile::Cost(state.cost),
+            Tile::Cost(_) => return,
+            Tile::End => {
+                match self.lowest_cost {
+                    None => self.lowest_cost = Some(state.cost),
+                    Some(x) if x > state.cost => self.lowest_cost = Some(state.cost),
+                    _ => (),
+                };
+            }
+        };
+
+        self.solve_for(state.move_forward());
+        self.solve_for(state.turn_left());
+        self.solve_for(state.turn_right());
+        self.solve_for(state.u_turn());
     }
 }
