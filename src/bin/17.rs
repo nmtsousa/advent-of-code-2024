cab7b62d39310b6bf83d9decc8f4f2de176cd30d
nmtsousa <nuno.mt.sousa@gmail.com>
2025-01-04 22:10:43 +0000

    Added inputs and first test.

---
+++ src/bin/17.rs
@@ -0,0 +1,54 @@
+use anyhow::*;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use adv_code_2024::*;
+
+const DAY: &str = "17";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+const TEST: &str = "\
+Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0
+";
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    fn part1<R: BufRead>(reader: R) -> Result<String> {
+        // TODO: Solve Part 1 of the puzzle
+        let answer = reader.lines().flatten().count();
+        Ok("answer".to_owned())
+    }
+
+    // TODO: Set the expected answer for the test input
+    assert_eq!("4,6,3,5,6,3,5,2,1,0", part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result);
+    //endregion
+
+    //region Part 2
+    // println!("\n=== Part 2 ===");
+    //
+    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
+    //     Ok(0)
+    // }
+    //
+    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
+    //
+    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    // let result = time_snippet!(part2(input_file)?);
+    // println!("Result = {}", result);
+    //endregion
+
+    Ok(())
+}
